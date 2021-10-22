452fd54941e7bdd52ced86d69f6c9bbb1b14d818
lorena_olay <olaylorena2002@gmail.com>
2021-10-22 13:04:44 +0200

    prueba

---
+++ src/main/java/BUCLE/EJ02.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author lorena
+ */
+public class EJ02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado=new Scanner(System.in);
+        int n,x;
+        System.out.print("Ingrese el valor final:");
+        n=teclado.nextInt();
+        x=1;
+        while (x<=n) {
+            System.out.print(x);
+            System.out.print(" - ");
+            x = x + 1;
+        }
+
+    }
+    
+}
---
+++ src/main/java/BUCLE/EJ03.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author lorena
+ */
+public class EJ03 {
+//Realizar un programa que imprima 25 términos de la serie 15 - 30 - 60 - 120, etc. 
+    //No se introducen valores por teclado.
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 15;
+        while (x <= 100) {
+            System.out.println(x + "br");
+            x = x * 2;
+            System.out.println(x);    
+
+        }
+
+    }
+
+}
