74d859818c169540ba669fd989b4bb45941dcc46
lorena_olay <olaylorena2002@gmail.com>
2021-11-04 10:34:19 +0100

    ole

---
+++ src/main/java/BUCLE/EJ09.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package BUCLE;
+
+/**
+ *
+ * @author lorena
+ */
+public class EJ09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i=0;
+        int j=6;
+        while (i<0) {
+            i++;
+            j++;
+        }
+        System.out.println(j);
+    }
+    
+}
---
+++ src/main/java/BUCLE/EJ10.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package BUCLE;
+
+/**
+ *
+ * @author lorena
+ */
+public class EJ10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Implementa un programa Java que permita visualizar todas las letras 
+        //minúsculas, desde la 'a' hasta la 'z', usando un for.
+        
+
+    }
+    
+}
